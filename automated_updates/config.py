85d6c2f45b21111d78eed1a5cd23c717434e0285
Lee Schmalz <chanceonbrink@gmail.com>
2024-11-13 15:36:29 -0600

    Update config.py

--- automated_updates/config.py
+++ automated_updates/config.py
@@ -1,4 +1,4 @@
-chrome_driver_path = ''
+chrome_driver_path = '' # download chromedriver from https://googlechromelabs.github.io/chrome-for-testing/, path to executable here
 
 bitcoin_crypto_terms = [
     'btc', 
@@ -8,15 +8,16 @@ bitcoin_crypto_terms = [
     'ethereum', 
     'grayscale', 
     'greyscale', 
-    'gbtc', 'arkb', 
+    'gbtc', 
+    'arkb', 
     'bitb', 
     'coinbase', 
     'microstrategy', 
     'mstr ',
-    'cryptocurrency'
+    'cryptocurrency',
+    'bito ',
 ]
 
 bitcoin_crypto_terms_false_positives = [
     'BTC LifePath',
-    'bito '
 ]
