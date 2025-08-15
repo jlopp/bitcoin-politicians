a428d28d226bce9121df44cd44dc80c2a3208f76
chance-on-brink <chanceonbrink@gmail.com>
2025-08-15 12:12:04 -0500

    upgrade to gpt-5

--- automated_updates/modules/process/openai_wrapper.py
+++ automated_updates/modules/process/openai_wrapper.py
@@ -13,7 +13,7 @@ def encode_image(image_path):
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 # @backoff.on_exception(backoff.expo, openai.RateLimitError)
-def send_to_api(message, base64_image, model='gpt-4.1-mini'):
+def send_to_api(message, base64_image, model='gpt-5-mini'):
     response = client.chat.completions.create(
         model=model,
         messages=[
